842b52d9b8188949cf14a10d3004036a2e4e90da
Alexander Kauer <contact@kaulex.dev>
2025-02-18 08:26:17 +0100

    NavigationDestination is now MainActor

--- Sources/Navigate/NavigationDestination.swift
+++ Sources/Navigate/NavigationDestination.swift
@@ -1,6 +1,7 @@
 import SwiftUI
 
 /// A type that represents a possible NavigationDestination in your app
+@MainActor
 public protocol NavigationDestination<Body>: Hashable, Identifiable {
     associatedtype Body: View
 
